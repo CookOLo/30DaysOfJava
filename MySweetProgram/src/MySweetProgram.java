7511f67399db6083393e7661a9a8996bcc0dc170
Dylan <dylanle1000@gmail.com>
2020-07-02 15:26:31 -0700

    initial commit

---
+++ MySweetProgram/src/MySweetProgram.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MySweetProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Enter you name");
+		
+		Scanner in = new Scanner(System.in);
+		//String s = in.nextLine();
+		
+		int i = 0;
+		
+		do {
+			System.out.println("i is: " + i);
+			i++;
+		}
+		while(i < 10);
+		
+		
+		
+		
+		in.close();
+	}
+}
