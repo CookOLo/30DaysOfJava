7f63aba07f2324e5b05f24b140cac8da8ffee16c
Dylan <dylanle1000@gmail.com>
2020-07-09 15:28:29 -0700

    working with 2d arrays

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -1,33 +1,21 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Scanner;
-import java.util.Stack;
-
 public class MySweetProgram {
 	
 
 	public static void main(String[] args) {
-
-		LinkedList<String> names = new LinkedList<String>();
-		names.push("Caleb");
-		names.push("Sue");
-		names.push("Sally");
-		
-		ListIterator<String> it = names.listIterator();   
-		it.next();
-		it.next();
-		it.add("Susan");
-		
-		for(String s: names) {
-			System.out.println(s);
-		}		
+		int [] [] data = {
+				{4, 6, 3, 10},
+				{5, 2, 40, 1, 5, 3, 2, 30},
+				{5, 34, 1, 43}
+		};
 	
+		
+		for(int i = 0; i < data.length; i++) {
+			for(int j = 0; j < data[i].length; j++) {
+				System.out.print(data[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
+	
 }
 
